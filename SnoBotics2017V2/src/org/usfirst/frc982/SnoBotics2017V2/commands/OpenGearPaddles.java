d34ee2ce331560b912e5548bf2ee1e8db86e06f3
Rob Steele <robsteele49@gmail.com>
2017-02-24 12:41:03 -0800

    Changes attempting to fix pneumatics.

--- SnoBotics2017V2/src/org/usfirst/frc982/SnoBotics2017V2/commands/OpenGearPaddles.java
+++ SnoBotics2017V2/src/org/usfirst/frc982/SnoBotics2017V2/commands/OpenGearPaddles.java
@@ -38,7 +38,7 @@ public class OpenGearPaddles extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     }
-
+    
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.gearManipulator.openPaddles();
@@ -46,11 +46,11 @@ public class OpenGearPaddles extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.gearManipulator.isPaddleOpen();
+    	return Robot.gearManipulator.isPaddleOpen();
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end() {	
     }
 
     // Called when another command which requires one or more of the same
