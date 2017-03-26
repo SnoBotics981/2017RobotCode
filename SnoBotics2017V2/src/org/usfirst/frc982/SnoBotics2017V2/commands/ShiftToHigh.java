960e0e750dd03f04f1a050dfc86dd28a8c27c41e
Rob Steele <robsteele49@gmail.com>
2017-03-25 17:35:20 -0700

    Working version from Saturday 3/25/17.

--- SnoBotics2017V2/src/org/usfirst/frc982/SnoBotics2017V2/commands/ShiftToHigh.java
+++ SnoBotics2017V2/src/org/usfirst/frc982/SnoBotics2017V2/commands/ShiftToHigh.java
@@ -53,12 +53,12 @@ public class ShiftToHigh extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	RobotMap.driveSystemSpikeForLights.set(Relay.Value.kForward);
+    	// RobotMap.driveSystemSpikeForLights.set(Relay.Value.kForward);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	RobotMap.driveSystemSpikeForLights.set(Relay.Value.kForward);
+    	// RobotMap.driveSystemSpikeForLights.set(Relay.Value.kForward);
     }
 }
